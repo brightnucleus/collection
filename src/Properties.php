b4b3529922875849607db81da77eabd0611841c2
Alain Schlesser <alain.schlesser@gmail.com>
2019-01-03 11:20:54 +0100

    Add Properties trait

---
+++ src/Properties.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Bright Nucleus Collection Post Collection
+ *
+ * @package   BrightNucleus\Collection
+ * @author    Alain Schlesser <alain.schlesser@gmail.com>
+ * @license   MIT+
+ * @link      http://www.brightnucleus.com/
+ * @copyright 2018 Alain Schlesser, Bright Nucleus
+ */
+
+namespace BrightNucleus\Collection;
+
+trait Properties {
+
+	/** @var PropertyCollection */
+	protected $properties;
+
+	/**
+	 * Get the collection of properties for the given Entity.
+	 *
+	 * @return PropertyCollection
+	 */
+	public function getProperties(): PropertyCollection {
+		return $this->properties;
+	}
+}
